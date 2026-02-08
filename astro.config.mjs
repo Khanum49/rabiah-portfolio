590b3a013979221b73713dd9bca7fc47db6d5fee
Khanum49 <khanumrabiah@gmail.com>
2026-02-08 21:00:55 +0500

    Fixed Issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
+// https://astro.build/config
 export default defineConfig({
-  site: "https://khanum49.github.io",
-  base: "/rabiah-portfolio",
-  output: "static",
-  integrations: [
-    tailwind(),
-    mdx()
-  ],
+  site: 'https://khanum49.github.io',
+  base: import.meta.env.PROD ? '/rabiah-portfolio' : '/',
+  integrations: [tailwind(), mdx()],
 });
