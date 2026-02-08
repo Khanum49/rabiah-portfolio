5f0472e6ff94143320abe714ae6a12d0982bc10a
Khanum49 <khanumrabiah@gmail.com>
2026-02-08 19:11:12 +0500

    Fix double docs path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
+  site: "https://khanum49.github.io",
+  base: "/rabiah-portfolio",
+  output: "static",
   integrations: [
     tailwind(),
     mdx()
