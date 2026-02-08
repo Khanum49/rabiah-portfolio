bea623516e5c69a16a62824de808ff9a787441fa
Khanum49 <khanumrabiah@gmail.com>
2026-02-08 21:13:28 +0500

    Astro config file updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://khanum49.github.io',
-  base: import.meta.env.PROD ? '/rabiah-portfolio' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/rabiah-portfolio' : '/',
   integrations: [tailwind(), mdx()],
 });
